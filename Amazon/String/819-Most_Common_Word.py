bcfc01fdc080016870c32940001c7ea7c4b67e87
Ankur Gokhale <ankurgokhale@Ankurs-MBP.home>
2020-09-30 20:36:15 -0500

    Changed directory of Most Common Word from Hashmap to String

--- Amazon/String/819-Most_Common_Word.py
+++ Amazon/String/819-Most_Common_Word.py
@@ -1,4 +1,5 @@
 '''
+String Manipulation Problem.
 The approach to this problem is that we have to take a hashmap to count the most common word. We need to take a hashset for the banned words. Replace special characters with space, then take lower case and split. Return max count
 
 Time Complexity: O(N)
@@ -13,11 +14,18 @@ class Solution:
         for ch in  "!?',;.":
             paragraph = paragraph.replace(ch, " ")
         paragraph = paragraph.lower().split()
-        
         for word in paragraph:
             if word not in banset:
-                if word in dict:
-                    dict[word]+=1
-                else:
+                if word not in dict:
                     dict[word] = 1
-        return max(dict,key = dict.get)
+                else:
+                    dict[word] += 1
+        return max(dict, key = dict.get)
+        
+
+                
+            
+
+
+                
+            
